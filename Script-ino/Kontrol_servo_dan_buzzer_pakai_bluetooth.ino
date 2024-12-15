53c2b907abfcd4b37e3e3e9615bccef30172702a
rizkysln_ <119398624+rizkysln@users.noreply.github.com>
2024-12-15 18:43:51 +0700

    Update Kontrol_servo_dan_buzzer_pakai_bluetooth.ino

--- Script-ino/Kontrol_servo_dan_buzzer_pakai_bluetooth.ino
+++ Script-ino/Kontrol_servo_dan_buzzer_pakai_bluetooth.ino
@@ -1,1 +1,46 @@
+#include <Servo.h>
+Servo myservo;
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
+#define klakson 12
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(klakson, OUTPUT);
+  digitalWrite(klakson,LOW);
+  myservo.attach(3);
+  myservo.write(0);
+  delay(1000);
+  myservo.detach();
+  delay(1000);
+}
+
+void loop() { // run over and over
+  if (mySerial.available())
+  {
+    char data = mySerial.read();
+    Serial.println(data);
+    switch (data)
+    {
+      case'A': myservo.attach(3);
+               myservo.write(90);
+               delay(500);
+               myservo.detach();
+               delay(500); 
+               break;
+      case'B': myservo.attach(3);
+               myservo.write(0);
+               delay(500);
+               myservo.detach();
+               delay(250); 
+               break;
+      case'H': digitalWrite(klakson,HIGH);
+               break;
+      case'I': digitalWrite(klakson,LOW);
+               break;        
+      default: break;
+    }
+  }
+}
 
